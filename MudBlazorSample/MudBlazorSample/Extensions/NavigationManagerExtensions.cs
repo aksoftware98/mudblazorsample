47a5e5eea8b5d824b70bd49029063b9258e64d97
Ahmad <aksoftware98@hotmail.com>
2021-02-08 21:57:38 +0200

    Initialize the project with MudBlazor

---
+++ MudBlazorSample/MudBlazorSample/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MudBlazorSample.Extensions
+{
+    public static class NavigationManagerExtensions
+    {
+        /// <summary>
+        /// Gets the section part of the documentation page
+        /// Ex: /components/button;  "components" is the section
+        /// </summary>
+        public static string GetSection(this NavigationManager navMan)
+        {
+            var currentUri = new Uri(navMan.Uri);
+            return currentUri.AbsolutePath
+                .Split("/", StringSplitOptions.RemoveEmptyEntries)
+                .FirstOrDefault();
+        }
+        /// <summary>
+        /// Gets the link of the component on the documentation page
+        /// Ex: /api/button; "button" is the component link, and "api" is the section
+        /// </summary>
+        public static string GetComponentLink(this NavigationManager navMan)
+        {
+            var currentUri = new Uri(navMan.Uri);
+            return currentUri.AbsolutePath
+                .Split("/", StringSplitOptions.RemoveEmptyEntries)
+                //the second element
+                .ElementAtOrDefault(1);
+        }
+
+        /// <summary>
+        /// Determines if the current page is the base page
+        /// </summary>
+        public static bool IsHomePage(this NavigationManager navMan)
+        {
+            return navMan.Uri == navMan.BaseUri;
+        }
+    }
+}
---
+++ MudBlazorSample/MudBlazorSample/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudBlazorSample
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // Register the Mud Services HERE 
+            builder.Services.AddMudServices(); 
+            
+            await builder.Build().RunAsync();
+        }
+    }
+}
